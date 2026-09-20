80765be5907efe2a3383787f2ca791a861997624
ronakkoradiya-dev <ronakkoradiyadev@gmail.com>
2026-09-20 14:04:41 -0400

    Complete exam_result_bootstrap.html template with pass/fail messaging and detailed choices feedback

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,9 +1,46 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Course, Enrollment, Question, Choice, Submission
 
-# Helper function to extract answers from request.POST
+class CourseListView(generic.ListView):
+    model = Course
+    template_name = 'onlinecourse/course_list_bootstrap.html'
+    context_object_name = 'course_list'
+
+class CourseDetailView(generic.DetailView):
+    model = Course
+    template_name = 'onlinecourse/course_details_bootstrap.html'
+    context_object_name = 'course'
+
+def registration_request(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("onlinecourse:index")
+    else:
+        form = UserCreationForm()
+    return render(request, "onlinecourse/user_registration_bootstrap.html", {"form": form})
+
+def login_request(request):
+    return redirect("onlinecourse:index")
+
+def logout_request(request):
+    logout(request)
+    return redirect("onlinecourse:index")
+
+def enroll(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    if user.is_authenticated:
+        Enrollment.objects.get_or_create(user=user, course=course)
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
 def extract_answers(request):
     submitted_choices = []
     for key, value in request.POST.items():
@@ -15,7 +52,6 @@ def extract_answers(request):
                 pass
     return submitted_choices
 
-# Submit view
 def submit(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     user = request.user
@@ -26,7 +62,6 @@ def submit(request, course_id):
     submission_id = submission.id
     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
 
-# Exam result view
 def show_exam_result(request, course_id, submission_id):
     context = {}
     course = get_object_or_404(Course, pk=course_id)
@@ -34,15 +69,14 @@ def show_exam_result(request, course_id, submission_id):
     choices = submission.choices.all()
 
     total_score = 0
-    questions = course.question_set.all()  # Assuming course has related questions
+    questions = course.question_set.all()
 
     for question in questions:
-        correct_choices = question.choice_set.filter(is_correct=True)  # Get all correct choices for the question
-        selected_choices = choices.filter(question=question)  # Get the user's selected choices for the question
+        correct_choices = question.choice_set.filter(is_correct=True)
+        selected_choices = choices.filter(question=question)
 
-        # Check if the selected choices are the same as the correct choices
         if set(correct_choices) == set(selected_choices):
-            total_score += question.grade  # Add the question's grade only if all correct answers are selected
+            total_score += question.grade
 
     context['course'] = course
     context['grade'] = total_score
